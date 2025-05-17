f5ab78cb237d0f9484b50380db403aaee7f36a77
PC <techarts0100@gmail.com>
2025-05-17 11:33:41 +0200

    fix test FareCalculatorServiceTest.java

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,11 +10,10 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        double inHours = ticket.getInTime().getTime();
+        double outHours = ticket.getOutTime().getTime();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        double duration = (outHours - inHours)/(1000*60*60);
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
